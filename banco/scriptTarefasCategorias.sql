0ef72c6ce48167181d6d4b2e76d26faa8091acd4
Professora Luciene Cavalcanti <lucienecavalcanti@live.com>
2025-05-19 21:38:05 -0300

    Update scriptTarefasCategorias.sql
    Tinham 2 primary key

--- banco/scriptTarefasCategorias.sql
+++ banco/scriptTarefasCategorias.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `categoria`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `categoria` (
-  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY key,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `descricao` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -46,7 +46,7 @@ DROP TABLE IF EXISTS `tarefa`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tarefa` (
-  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY key,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `descricao` text DEFAULT NULL,
   `dataHoraCriacao` datetime DEFAULT NULL,
   `dataHoraTermino` datetime DEFAULT NULL,
